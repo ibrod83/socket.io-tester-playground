06e9c09f0ddd76b12b5f1e37e27353442b175379
Igal Brodsky <ibrod1983@gmail.com>
2019-02-24 19:50:15 +0200

    fixed git ignore

---
+++ build/precache-manifest.7b1d254bc6b5b0ce3181b14b0b1fd92a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "d72676a2cebeb116dcf4",
+    "url": "./static/js/main.d72676a2.chunk.js"
+  },
+  {
+    "revision": "0dc34255306f88ca3fdd",
+    "url": "./static/js/2.0dc34255.chunk.js"
+  },
+  {
+    "revision": "d72676a2cebeb116dcf4",
+    "url": "./static/css/main.46ffe717.chunk.css"
+  },
+  {
+    "revision": "6fee0bfb9a13e2d447f0a7868ff8cb0f",
+    "url": "./index.html"
+  }
+];
